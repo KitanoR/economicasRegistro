046d7c903319a77d3e0b48eda4ab1a937e21d76f
Cayetano Rosales <cayeroza@gmail.com>
2019-10-13 12:25:01 -0600

    cambiando correo

--- app/settings.py
+++ app/settings.py
@@ -155,8 +155,8 @@ MEDIA_URL = '/media/'
 # Email Config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'qetamajapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'qetamaj123'
+EMAIL_HOST_USER = 'facultadeconomicasmeso@gmail.com'
+EMAIL_HOST_PASSWORD = 'Administracion19'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
