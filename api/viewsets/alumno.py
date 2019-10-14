b4e2912b505c8623c4eb01aaa28f62f83f1987f2
Cayetano Rosales <cayeroza@gmail.com>
2019-10-13 23:54:15 -0600

    quitando espacios

--- api/viewsets/alumno.py
+++ api/viewsets/alumno.py
@@ -88,11 +88,8 @@ class AlumnoViewset(viewsets.ModelViewSet):
             ).save()
             silla.estado_lugar = Silla.OCUPADO
             silla.save()
-            
             """
-           
             codigo = str(alumno.codigo_qr.url)
-            
             orden_correo = {
                 'silla': '{}-{}'.format(silla.fila_letra, silla.no_lugar),
                 'usuario': alumno.nombre,
