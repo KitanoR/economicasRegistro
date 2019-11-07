15a2ef910085ab3ba5e6ac24db331527226e3c70
Corina Nimatuj <cori@ciancoders.com>
2019-11-07 12:45:41 -0600

    Parametros opcionales inicio y fin

--- api/management/commands/enviarQR.py
+++ api/management/commands/enviarQR.py
@@ -11,19 +11,48 @@ from django.core.files import File
 import qrcode
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--inicio', nargs='?', 
+            help='Pk  donde inicia el envío de correos',
+            type=int
+        )
+        parser.add_argument(
+            '--fin', nargs='?', 
+            help='Pk  donde finaliza el envío de correos',
+            type=int
+        )
+
+
+          
+
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Ejecutando envíos...'))
-        self.recorrerAsignaciones()
+        inicio =  options['inicio'] if options['inicio'] else 0
+        fin =  options['fin'] if options['fin'] else 0
+        # print(fin)
+        self.recorrerAsignaciones(inicio, fin)
 
 
-    def recorrerAsignaciones(self):
-        asignaciones = Asignacion.objects.filter(activo=True)
+    def recorrerAsignaciones(self, inicio, fin):
+        filters = {'activo': True}
+        if(inicio>0 and fin>0):
+            filters['pk__range'] = (inicio, fin)
+            self.stdout.write(self.style.SUCCESS('Enviando correos desde {} hasta {} ...'.format(inicio, fin)))
+        elif(inicio>0):
+            filters['pk__gte'] = int(inicio)
+            self.stdout.write(self.style.SUCCESS('Enviando correos desde {}...'.format(inicio)))
+        elif(fin>0):
+            filters['pk__lte'] = int(fin)
+            self.stdout.write(self.style.SUCCESS('Enviando correos hasta {}...'.format(fin)))
+
+        asignaciones = Asignacion.objects.filter(**filters)
         for asignacion in asignaciones:
             self.sendEmailQR(asignacion)
             self.stdout.write(self.style.SUCCESS('Enviando...'))
         else:
-            self.stdout.write(self.style.SUCCESS('Finalizado. Ha enviado 0 correos.'))
+            self.stdout.write(self.style.SUCCESS('Finalizado.'))
 
 
     def sendEmailQR(self, asignacion):
